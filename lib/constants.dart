6511dc58ae15d84e960635a86392f930ae9ebb28
Salman <salman.naeem.112215@gmail.com>
2023-01-05 19:00:12 +0500

    Added Half

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFF7643);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryGradientColor = LinearGradient(
+  colors: [Color(0xffffa53e), Color(0xffff7643)],
+);
